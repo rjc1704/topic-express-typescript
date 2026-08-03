fc17f829bb2b84ee411e458a42a3e86de1aef6bb
Jecheon Ryu <rjc1704@gmail.com>
2026-08-04 02:21:54 +0900

    수정

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -3,7 +3,7 @@ import { User as PrismaUser } from "@prisma/client";
 declare global {
   namespace Express {
     export interface Request {
-      auth: {
+      auth?: {
         userId: string;
       };
       user?: PrismaUser; // passport가 추가하는 user 속성
